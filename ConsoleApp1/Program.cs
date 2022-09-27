8cd2e7e06e2be058229a97529150b3055b8ced36
CrusaderKleptoman <64123771+CrusaderKleptoman@users.noreply.github.com>
2022-09-28 00:15:01 +0800

    Начало работы
    Создание репозитория. Задача выполнена, автотесты пройдены

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Logic
+    {
+        public static string Compare(int timeTrainArrival, int timeTrainDeparture, int timePassenger)
+        {
+            string outMessage = "";
+            if (timeTrainArrival > timeTrainDeparture)
+            {
+                timeTrainDeparture += 24*60;
+            }
+
+            if (timeTrainArrival <= timePassenger && timePassenger <= timeTrainDeparture)
+            {
+                outMessage = "Поезд будет стоять на платформе во время прихода пассажира";
+            }
+            else
+            {
+                outMessage = "Поезд не будет стоять на платформе во время прихода пассажира";
+            }
+
+            return outMessage;
+        }
+
+        public static int GetTime(int hour, int minutes)
+        {
+            int time = hour * 60 + minutes;
+
+            return time;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a, b, c, d, n, m;
+
+            Console.WriteLine("Введите время прибытия поезда");
+            Console.WriteLine("Час прибытия поезда");
+            a = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Минута прибытия поезда");
+            b = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите время отправки поезда");
+            Console.WriteLine("Час отправки поезда");
+            c = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Минута отправки поезда");
+            d = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите время прихода пассажира");
+            Console.WriteLine("Час прихода пассажира");
+            n = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Минута прихода пассажира");
+            m = byte.Parse(Console.ReadLine());
+
+            var timeTrainArrival = Logic.GetTime(a, b);
+            var timeTrainDeparture = Logic.GetTime(c, d);
+            var timePassenger = Logic.GetTime(n, m);
+            var outMessage = Logic.Compare(timeTrainArrival, timeTrainDeparture, timePassenger);
+
+            Console.WriteLine(outMessage);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp1Tests/LogicTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void PassengerCameWhileTrainWasParked()
+        {
+            var a = 1; var b = 0;
+            var c = 3; var d = 5;
+            var n = 1; var m = 50;
+
+            var timeTrainArrival = Logic.GetTime(a, b);
+            var timeTrainDeparture = Logic.GetTime(c, d);
+            var timePassenger = Logic.GetTime(n, m);
+            var message = Logic.Compare(timeTrainArrival, timeTrainDeparture, timePassenger);
+
+            Assert.AreEqual(60, timeTrainArrival);
+            Assert.AreEqual(185, timeTrainDeparture);
+            Assert.AreEqual(110, timePassenger);
+            Assert.AreEqual("Поезд будет стоять на платформе во время прихода пассажира", message);
+
+        }
+
+        [TestMethod()]
+        public void PassengerCameWhileTrainWasNotParked()
+        {
+            var a = 3; var b = 7;
+            var c = 4; var d = 30;
+            var n = 5; var m = 20;
+
+            var timeTrainArrival = Logic.GetTime(a, b);
+            var timeTrainDeparture = Logic.GetTime(c, d);
+            var timePassenger = Logic.GetTime(n, m);
+            var message = Logic.Compare(timeTrainArrival, timeTrainDeparture, timePassenger);
+
+            Assert.AreEqual(187, timeTrainArrival);
+            Assert.AreEqual(270, timeTrainDeparture);
+            Assert.AreEqual(320, timePassenger);
+            Assert.AreEqual("Поезд не будет стоять на платформе во время прихода пассажира", message);
+        }
+    }
+}
